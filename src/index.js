9fb230d1fb1fa5860ab8da7d0a4e9c9e405685f9
spencer.trinh <spencer.trinh@kinnate.com>
2023-07-05 16:49:21 -0700

    remove strict mode due to double render on mount

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route exact path='/sarView' element={<MainView />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route exact path='/sarView' element={<MainView />} />
+    </Routes>
+  </BrowserRouter>
 )
