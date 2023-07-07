13d95da245e6b95187f9321f6429fd76738a91ec
spencer.trinh <spencer.trinh@kinnate.com>
2023-07-07 12:28:05 -0700

    use session storage; change fetchData

--- src/MainView.tsx
+++ src/MainView.tsx
@@ -3,7 +3,7 @@ import ReactLoading from 'react-loading'
 import { colour } from './Colour'
 import { TableDataType } from './types'
 import TableGrid from './TableGrid.tsx'
-import { AxiosResponse, AxiosError } from 'axios'
+import { AxiosResponse } from 'axios'
 import axios from 'axios'
 import Box from '@mui/material/Box'
 import GoHomeIcon from './GoHomeIcon'
@@ -45,52 +45,63 @@ const MainView: React.FC = () => {
   const [disablePagination, setDisablePagination] = useState<boolean>(true)
   const compoundsPerPage = 10
 
-  const fetchData = async (url: string, updateRequestIds: boolean) => {
+  const fetchData = async (url: string, compoundIdsArray: Array<string>) => {
     setLoading(true)
-    await axios
-      .get(url, axiosConfig)
-      .then(async (res: AxiosResponse<any>) => {
-        const json = res.data
-        if (
-          typeof BACKEND_URL !== 'undefined' &&
-          BACKEND_URL.match(/localhost/g)
-        ) {
-          console.log(json)
+    try {
+      let response: AxiosResponse<any>
+
+      if (compoundIdsArray.length === 0) {
+        response = await axios.get(url, axiosConfig)
+      } else {
+        response = await axios.post(
+          url,
+          { compound_ids: compoundIdsArray },
+          axiosConfig
+        )
+      }
+      const json = response.data
+      if (
+        typeof BACKEND_URL !== 'undefined' &&
+        BACKEND_URL.match(/localhost/g)
+      ) {
+        console.log(json)
+      }
+      if (response.status === 200) {
+        if (compoundIdsArray.length > 0) {
+          setRequestIds(json.request_ids)
         }
-        if (res.status === 200) {
-          if (updateRequestIds) {
-            setRequestIds(json.request_ids)
-          }
-          setTableData(json.data)
-          setLoading(false)
-        }
-      })
-      .catch((err: AxiosError<any>) => {
-        console.log('AXIOS ERROR: ', err)
-      })
+        setTableData(json.data)
+        setLoading(false)
+      }
+    } catch (err) {
+      console.log('AXIOS ERROR: ', err)
+      setLoading(false)
+    }
   }
 
   useEffect(() => {
     const fetchInitialData = async () => {
       const urlParams = new URLSearchParams(window.location.search)
       const urlParamsObj = Object.fromEntries(urlParams)
-      console.log(urlParams.toString())
+      // console.log(urlParams.toString())
 
       if (
         !urlParamsObj.hasOwnProperty('date_filter') ||
-        !urlParamsObj.hasOwnProperty('compound_id')
+        !urlParamsObj.hasOwnProperty('session_id')
       ) {
-        console.error('Error: Date or compound ID is missing!')
+        console.error('Error: Date or session ID is missing!')
       }
       const dateFilter = urlParamsObj.date_filter
-      const compoundIdParam = urlParamsObj.compound_id
-      const compoundIdsArray = compoundIdParam.split('-')
+      const sessionIdParam = urlParamsObj.session_id
+
+      const compoundIdsString = sessionStorage.getItem(sessionIdParam)
+      const compoundIdsArray = compoundIdsString.split('-')
       compoundIdSort(compoundIdsArray)
-      console.log(compoundIdsArray)
+      // console.log(compoundIdsArray)
       setCompoundIds(compoundIdsArray)
-      const initialUrl = `${BACKEND_URL}/v1/sar_view_sql_hset?date_filter=${dateFilter}&compound_ids=${compoundIdParam}`
-      console.log(initialUrl)
-      await fetchData(initialUrl, true)
+      const initialUrl = `${BACKEND_URL}/v1/sar_view_sql_hset?date_filter=${dateFilter}`
+      // console.log(initialUrl)
+      await fetchData(initialUrl, compoundIdsArray)
     }
 
     const fetchPaginatedData = async () => {
@@ -98,7 +109,7 @@ const MainView: React.FC = () => {
         requestIds[page - 1]
       }`
       console.log(paginatedUrl)
-      await fetchData(paginatedUrl, false)
+      await fetchData(paginatedUrl, [])
     }
 
     if (initialFetch) {
