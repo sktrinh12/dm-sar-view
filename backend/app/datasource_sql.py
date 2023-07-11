57eb9ceb3c77d49fc0be6dbe02e87bdb5cce498a
spencer.trinh <spencer.trinh@kinnate.com>
2023-07-10 19:22:25 -0700

    pass payload to function instead

--- backend/app/datasource_sql.py
+++ backend/app/datasource_sql.py
@@ -2,11 +2,8 @@ import requests
 from .backend_url import BACKEND_URL
 
 
-def get_ds_sql():
+def get_ds_sql(payload):
     url = f"http://{BACKEND_URL}/v1/fetch_ds_query"
-    # this needs to match dictionary.py key
-    ds_name = "biochem"
-    payload = {ds_name: {"id": 912, "app_type": "sar"}}
 
     response = requests.get(url, json=payload)
     if response.status_code == 200:
