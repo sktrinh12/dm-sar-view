647c9664eada5e800d9cee0c28aae1d4a0b247e8
spencer.trinh <spencer.trinh@kinnate.com>
2023-07-11 17:35:28 -0700

    need nested loop to extract ts

--- backend/app/background_task.py
+++ backend/app/background_task.py
@@ -9,12 +9,13 @@ def purge_expired_keys():
     while True:
         current_time = datetime.now()
         keys_to_remove = []
-        for key, *_, timestamp in remaining_batches.items():
-            timestamp_obj = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
-            if (
-                current_time - timestamp_obj > duration
-            ):  # Check if timestamp is older than 5 hours
-                keys_to_remove.append(key)
+        for key, lst in remaining_batches.items():
+            for *_, timestamp in lst:
+                timestamp_obj = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+                if (
+                    current_time - timestamp_obj > duration
+                ):  # Check if timestamp is older than 5 hours
+                    keys_to_remove.append(key)
         for key in keys_to_remove:
             print(f"deleting batch key, {key}")
             del remaining_batches[key]
