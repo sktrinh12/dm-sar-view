3b3575baa9991c73201e815379191cd05f9748c4
spencer.trinh <spencer.trinh@kinnate.com>
2023-07-28 12:58:46 -0700

    separate exec bkg fx using threads

--- backend/app/api.py
+++ backend/app/api.py
@@ -3,26 +3,28 @@ from fastapi import HTTPException
 from fastapi.responses import JSONResponse
 from fastapi import BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
-import threading
-import queue
 from math import ceil
 import uuid
 from datetime import datetime, timedelta
-from .oracle_class import OraclePoolCxn
-from .credentials import cred_dct
 from json import loads, dumps
-from .functions import execute_query_background_redis
+from .functions import (
+    execute_query_background_redis_celery,
+    execute_query_background_redis_thread,
+)
 from .redis_connection import redis_conn
 from .sql import dm_table_cols, sql_stmts
 from .globals import remaining_batches
+import queue
+import threading
 from .background_task import purge_expired_keys
 from .datasource_sql import get_ds_sql
-from .worker_count import workers
+from .credentials import ENV
+from .credentials import cred_dct
+from .oracle_class import OracleCxn
 from os import getenv
 
 
 app = FastAPI()
-pool = None
 
 origins = [
     "http://localhost:3000",
@@ -38,45 +40,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-query_results_lock = threading.Lock()
 q = queue.Queue()
-ENV = getenv("ENV", "DEV")
 background_purge = threading.Thread(target=purge_expired_keys, daemon=True)
 background_purge.start()
 
 
 def startup_event():
-    global pool
-    pool = OraclePoolCxn(
-        cred_dct["HOST"] if ENV == "PROD" else cred_dct["HOST-DEV"],
-        cred_dct["PORT"],
-        cred_dct["SID"],
-        cred_dct["USERNAME"],
-        cred_dct["PASSWORD"],
-    )
-    pool.connect()
     print("Server startup")
     payload = {"biochemical": {"id": 912, "app_type": "geomean_sar"}}
     sql = get_ds_sql(payload)
     sql_stmts["biochemical_geomean"] = sql["0"]["formatted_query"]
     print("Updated biochemical geomean sql")
-
-
-def shutdown_event():
-    global pool
-    if pool is not None:
-        pool.disconnect()
-    print("Server shutdown")
+    if getenv("ORACLE_HOST") is None:
+        print(sql_stmts["biochemical_geomean"])
 
 
 @app.on_event("startup")
 async def startup():
     startup_event()
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    shutdown_event()
 
 
 @app.get("/")
@@ -131,21 +112,28 @@ async def fetch_cmpid_from(
     cmpd_ids = []
     if dm_table:
         col_query = dm_table_cols.format(dm_table)
-        column_name = pool.execute(col_query)  # ignore member-error
+        cxn = OracleCxn(
+            cred_dct["HOST"],
+            cred_dct["PORT"],
+            cred_dct["SID"],
+            cred_dct["USERNAME"],
+            cred_dct["PASSWORD"],
+        )
+        column_name = cxn.execute(col_query)  # ignore member-error
         if ENV != "PROD":
             print(col_query)
         if column_name:
             column_name = column_name[0][0]
             fetch_query = f"SELECT {column_name} AS cmpd_id FROM {dm_table}"
-            rtn_data = pool.execute(fetch_query)  # ignore member-error
+            rtn_data = cxn.execute(fetch_query)  # ignore member-error
             for cid in rtn_data:
                 cmpd_ids.append(cid[0])
     return Response(content=dumps(cmpd_ids))
 
 
 # get request id from redis for pagination
-@app.get("/v1/sar_view_sql_hget")
-async def hget_redis(request_id: str):
+@app.get("/v1/sar_view_sql_get")
+async def sar_view_sql_get(request_id: str):
     results_available = redis_conn.exists(request_id)
     if not results_available:
         raise HTTPException(status_code=404, detail="No query results available")
@@ -156,14 +144,34 @@ async def hget_redis(request_id: str):
     )
 
 
+@app.post("/v1/sar_view_sql_update_biochem")
+async def sar_view_sql_update_biochem(
+    request_id: str,
+    updated_data: dict = Body(...),
+):
+    results_available = redis_conn.exists(request_id)
+    if not results_available:
+        raise HTTPException(
+            status_code=404,
+            detail=f"request ID is wrong {request_id}, couldn't update biochem data",
+        )
+    # print(updated_data)
+    result = redis_conn.set(request_id, dumps(updated_data["updatedData"]))
+    return JSONResponse(
+        content={"request_id": request_id, "status": result},
+        media_type="application/json",
+    )
+
+
 # set request ids and trigger background task
-@app.post("/v1/sar_view_sql_hset")
-async def hset_redis(
+# fast_type {1: all, 0: fast, -1: slow}
+@app.post("/v1/sar_view_sql_set")
+async def sar_view_sql_set(
     background_tasks: BackgroundTasks,
     request_data: dict = Body(...),
-    max_workers: int = Query(default=workers),
     user: str = Query(default="TESTADMIN"),
     pages: int = Query(default=10),
+    fast_type: int = Query(default=0, choices=[-1, 0, 1]),
     date_filter: str = Query(
         (
             f'{(datetime.now() - timedelta(days=7)).strftime("%m-%d-%Y")}'
@@ -172,6 +180,9 @@ async def hset_redis(
     ),
 ):
     compound_ids = request_data.get("compound_ids")
+    print(
+        f"Query type: {fast_type} {'all' if fast_type == 1 else 'slow' if fast_type == -1 else 'fast'}"
+    )
     if compound_ids is not None:
         print(f"compound id count: {len(compound_ids)}")
     else:
@@ -185,37 +196,51 @@ async def hset_redis(
     request_ids = []
     request_id = f"{str(uuid.uuid4())}_page_1"
     request_ids.append(request_id)
-    execute_query_background_redis(
-        pool,
-        q,
-        query_results_lock,
-        request_id,
-        compound_ids[:pages],
-        start_date,
-        end_date,
-        max_workers,
-    )
-    results = redis_conn.get(request_id)
-    if nbr_cmpds > pages:
+    # print(f"first batch: { compound_ids[:pages]}")
+    if fast_type == 0:
+        data = execute_query_background_redis_thread(
+            q,
+            request_id,
+            compound_ids[:pages],
+            start_date,
+            end_date,
+            fast_type,
+        )
+    else:
+        data = execute_query_background_redis_celery(
+            request_id,
+            compound_ids[:pages],
+            start_date,
+            end_date,
+            fast_type,
+        )
+    if fast_type == -1:
+        return JSONResponse(
+            content={
+                "request_ids": request_ids,
+                "data": data,
+            },
+            media_type="application/json",
+        )
+    # results = redis_conn.get(request_id)
+    if nbr_cmpds > pages and fast_type != -1:
         remaining_batches[f"{user}_batch"] = []
         num_batches = ceil((nbr_cmpds - pages) / pages)
         for i in range(num_batches):
             start_idx = pages + i * pages
             end_idx = pages + (i + 1) * pages
             subset_compound_ids = compound_ids[start_idx:end_idx]
+            # print(f"next batch running for ... {subset_compound_ids}")
             request_id = f"{str(uuid.uuid4())}_page_{i+2}"
             request_ids.append(request_id)
             if i < pages:
                 background_tasks.add_task(
-                    execute_query_background_redis,
-                    pool,
-                    q,
-                    query_results_lock,
+                    execute_query_background_redis_celery,
                     request_id,
                     subset_compound_ids,
                     start_date,
                     end_date,
-                    max_workers,
+                    1,
                 )
             else:
                 remaining_batches[f"{user}_batch"].append(
@@ -223,12 +248,11 @@ async def hset_redis(
                         request_id,
                         start_date,
                         end_date,
-                        max_workers,
                         subset_compound_ids,
                         datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                     )
                 )
-    data = loads(results.decode()) if results is not None else None
+    # data = loads(results.decode()) if results is not None else None
     return JSONResponse(
         content={
             "request_ids": request_ids,
@@ -252,20 +276,21 @@ async def next_batch(
         batches = remaining_batches[key][:pages]
         remaining_batches[key] = remaining_batches[key][pages:]
         for b in batches:
-            (request_id, start_date, end_date, max_workers, compound_ids_batch, _) = b
+            (request_id, start_date, end_date, compound_ids_batch, _) = b
             rtn_request_id.append(request_id)
             rtn_compound_ids_batch.append(compound_ids_batch)
             background_tasks.add_task(
-                execute_query_background_redis,
-                pool,
-                q,
-                query_results_lock,
+                execute_query_background_redis_celery,
                 request_id,
                 compound_ids_batch,
                 start_date,
                 end_date,
-                max_workers,
+                1,
             )
+    else:
+        if f"{user}_batch" in remaining_batches:
+            print(f"{datetime.now()} deleting batch key, {user}_batch")
+            del remaining_batches[f"{user}_batch"]
     return JSONResponse(
         content={
             "request_ids": rtn_request_id,
@@ -278,7 +303,7 @@ async def next_batch(
 # show remaining batches for all users
 @app.get("/v1/get_remaining_batches")
 async def get_batches():
-    return JSONResponse(content={"batches": remaining_batches})
+    return JSONResponse(content=remaining_batches)
 
 
 # cancel batches of 100 for specified user
