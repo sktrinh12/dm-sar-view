6d558290ca602b43df5acd19e2c59964a4f4da68
spencer.trinh <spencer.trinh@kinnate.com>
2023-07-28 13:00:03 -0700

    0th indez on list; add print()

--- backend/app/background_task.py
+++ backend/app/background_task.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from time import sleep
 from .globals import remaining_batches
 
-duration = timedelta(hours=5)
+duration = timedelta(seconds=3600)
 
 
 def purge_expired_keys():
@@ -11,15 +11,14 @@ def purge_expired_keys():
         keys_to_remove = []
         remaining_batches_copy = remaining_batches.copy()
         for key, lst in remaining_batches_copy.items():
+            # print(f"{key} - {lst}")
             if lst:
-                for *_, timestamp in lst:
+                for *_, timestamp in lst[0]:
+                    print(f"background purge timestamp: {timestamp}")
                     timestamp_obj = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
-                    if (
-                        current_time - timestamp_obj > duration
-                    ):  # Check if timestamp is older than 5 hours
+                    if current_time - timestamp_obj > duration:
                         keys_to_remove.append(key)
             else:
-                # empty batches
                 keys_to_remove.append(key)
         for key in keys_to_remove:
             print(f"{current_time} deleting batch key, {key}")
